7f96c46090d2c4cc67198f774a164d40acc955fb
amr-mahmoud <amrhazem@rubix-studio.com>
2021-11-21 22:47:40 +0200

    Finish this

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -3,27 +3,58 @@ import * as actions from "./index";
 
 jest.mock("axios");
 
-const mockres = {
-  total_count: 50,
-  data,
-};
-
+const repos = [
+  {
+    id: "1",
+    name: "namme1",
+    language: "Python",
+    url: "ww.giturl",
+    description: "description",
+    starred: false,
+  },
+  {
+    id: "2",
+    name: "namme2",
+    language: "js",
+    url: "ww.giturl2",
+    description: "description2",
+    starred: false,
+  },
+];
 beforeEach(() => {
   jest.mock("axios");
 });
 
-it("getArticleById should output correct format", async () => {
+it("getRepoList should output correct format", async () => {
   axios.get = jest.fn().mockImplementationOnce(() =>
     Promise.resolve({
       status: 200,
-      data: { response: { data: { items: [], total_count: 2 }, status: 200 } },
+
+      data: {
+        items: [
+          {
+            id: "1",
+            name: "namme1",
+            language: "Python",
+            git_url: "ww.giturl",
+            description: "description",
+          },
+          {
+            id: "2",
+            name: "namme2",
+            language: "js",
+            git_url: "ww.giturl2",
+            description: "description2",
+          },
+        ],
+        total_count: 2,
+      },
+      status: 200,
     })
   );
 
-  expect(actions.getRepoList(0, "", "desc", "any")).resolves.toEqual({
-    headline: "USAA Offers Service for Car Shoppers",
-    pub_date: "2010-08-31T11:30:02+0000",
-    description: "USAA offers a car-buying service and iPhone app.,",
-    id: "fakeId",
+  await expect(actions.getRepoList(0, "", "desc", "any")).resolves.toEqual({
+    repos,
+    total_count: 2,
   });
 });
