cc29d848cfa9242d0d74ef10eafa05303b75bd5e
amr-mahmoud <amrhazem@rubix-studio.com>
2021-11-22 16:34:44 +0200

    fix s styled components import issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Home from "./components/Home";
 import { AppProvider } from "./provider";
 
-const App: React.FC = () => (
+const App = () => (
   <AppProvider>
     <Home />
   </AppProvider>
--- src/components/Select/Select.style.ts
+++ src/components/Select/Select.style.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const SelectContainer = styled.div`
   width: 140px;
